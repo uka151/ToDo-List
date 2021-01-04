91c78194b77624c470fae6434a537310ba946f4f
Umesh Kumar Agrahari <50727393+uka151@users.noreply.github.com>
2021-01-04 23:01:06 +0530

    Intial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.srnotxt = new System.Windows.Forms.TextBox();
+            this.sr = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.todoListDataSet = new WindowsFormsApplication1.TodoListDataSet();
+            this.listBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listTableAdapter = new WindowsFormsApplication1.TodoListDataSetTableAdapters.listTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.deltxt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.todoListDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textContent
+            // 
+            this.textContent.Location = new System.Drawing.Point(595, 192);
+            this.textContent.Multiline = true;
+            this.textContent.Name = "textContent";
+            this.textContent.Size = new System.Drawing.Size(452, 134);
+            this.textContent.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(625, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 82);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TodoList";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(1137, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ADD TASK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // srnotxt
+            // 
+            this.srnotxt.Location = new System.Drawing.Point(595, 134);
+            this.srnotxt.Multiline = true;
+            this.srnotxt.Name = "srnotxt";
+            this.srnotxt.Size = new System.Drawing.Size(100, 36);
+            this.srnotxt.TabIndex = 3;
+            // 
+            // sr
+            // 
+            this.sr.AutoSize = true;
+            this.sr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sr.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.sr.Location = new System.Drawing.Point(474, 134);
+            this.sr.Name = "sr";
+            this.sr.Size = new System.Drawing.Size(80, 29);
+            this.sr.TabIndex = 4;
+            this.sr.Text = "SrNo.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button3.Location = new System.Drawing.Point(595, 629);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "VIEW";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // todoListDataSet
+            // 
+            this.todoListDataSet.DataSetName = "TodoListDataSet";
+            this.todoListDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // listBindingSource
+            // 
+            this.listBindingSource.DataMember = "list";
+            this.listBindingSource.DataSource = this.todoListDataSet;
+            // 
+            // listTableAdapter
+            // 
+            this.listTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.Location = new System.Drawing.Point(595, 455);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(452, 150);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(460, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Message";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button4.Location = new System.Drawing.Point(1137, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 50);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "DEL TASK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // deltxt
+            // 
+            this.deltxt.Location = new System.Drawing.Point(595, 346);
+            this.deltxt.Multiline = true;
+            this.deltxt.Name = "deltxt";
+            this.deltxt.Size = new System.Drawing.Size(452, 50);
+            this.deltxt.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 730);
+            this.Controls.Add(this.deltxt);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.sr);
+            this.Controls.Add(this.srnotxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textContent);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "TodoList";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.todoListDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox srnotxt;
+        private System.Windows.Forms.Label sr;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private TodoListDataSet todoListDataSet;
+        private System.Windows.Forms.BindingSource listBindingSource;
+        private TodoListDataSetTableAdapters.listTableAdapter listTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox deltxt;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=TodoList;Integrated Security=True");
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'todoListDataSet.list' table. You can move, or remove it, as needed.
+            this.listTableAdapter.Fill(this.todoListDataSet.list);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String query = "insert into list(id,msg) VALUES('" + srnotxt.Text + "','" + textContent.Text + "')";
+            SqlCommand cmd = new SqlCommand(query,con); 
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Task Added in your List");
+            this.srnotxt.Text = "";
+            this.textContent.Text = "";
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string qry = "select *from list";
+            SqlDataAdapter adb = new SqlDataAdapter(qry, con);
+            DataTable dt = new DataTable();
+            adb.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "delete from list where id = '" + deltxt.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            int i = cmd.ExecuteNonQuery();
+            if (i >= 1)
+            {
+                MessageBox.Show("Deleted information !!!!!");
+            }
+            else
+            {
+                MessageBox.Show("Failed to Delete Data!!!!!!!!!!");
+            }
+
+            con.Close();
+        }
+
+      
+
+        
+        }
+
+        
+
+    }
+
